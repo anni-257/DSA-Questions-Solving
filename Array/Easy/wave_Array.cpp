b86cea2d6e3b351ca25da5955872e74bcfab200e
anni-257 <taraleaniket257@gmail.com>
2022-07-19 22:12:50 +0530

    GFG bitonic Point Question

---
+++ Array/Easy/wave_Array.cpp
@@ -0,0 +1,17 @@
+// https://practice.geeksforgeeks.org/problems/wave-array-1587115621/1?page=1&curated[]=8&sortBy=submissions
+
+void convertToWave(int n, vector<int>& arr){
+        
+        if(n%2==0){
+            n=n-1;
+        }
+        else{
+            n=n-2;
+        }
+        
+        for(int i=0;i<n;i+=2){
+            swap(arr[i],arr[i+1]);
+        }
+        
+        
+    }
