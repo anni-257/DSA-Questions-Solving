9e72ae4f38aa961d84eb293162f51d8056b062b9
anni-257 <taraleaniket257@gmail.com>
2022-07-22 21:28:00 +0530

    Leetcode 350. Intersection of Two Arrays II

---
+++ Array/Medium/intersectionII.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/intersection-of-two-arrays-ii/
+
+vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        
+        int i=0,j=0;
+        vector<int> ans;
+        
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else if(nums2[j]<nums1[i]){
+                j++;
+            }
+            else{
+                ans.emplace_back(nums1[i++]);
+                j++;
+            }
+        }
+        return ans;
+    }
