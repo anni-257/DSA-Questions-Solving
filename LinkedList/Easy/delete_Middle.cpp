8bcac4c583ec5f9906a33353e35dc36881719259
anni-257 <taraleaniket257@gmail.com>
2022-07-30 20:45:50 +0530

    GFG Delete Middle of Linked List

---
+++ LinkedList/Easy/delete_Middle.cpp
@@ -0,0 +1,39 @@
+
+// https://practice.geeksforgeeks.org/problems/delete-middle-of-linked-list/1?page=2&category[]=Linked%20List&sortBy=submissions
+
+
+// Deletes middle of linked list and returns head of the modified list
+
+int count(Node* head){
+    int cnt=0;
+    while(head){
+        cnt++;
+        head=head->next;
+    }
+    return cnt;
+    
+}
+Node* deleteMid(Node* head)
+{
+    Node* p=head,*q;
+    int cnt=count(head),mid=0;
+    if(!head || cnt==1){
+        return NULL;
+    }else{
+        
+        mid=cnt/2;
+        
+        for(int i=0;i<mid && p;i++){
+            q=p;
+            p=p->next;
+        }
+        
+        if(p){
+            q->next=p->next;
+            delete p;
+        }
+        
+    }
+    
+    return head;
+}
