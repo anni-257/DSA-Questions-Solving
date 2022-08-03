7651aa52779e0167fa5595845623c9d39b3e4a30
anni-257 <taraleaniket257@gmail.com>
2022-08-03 18:49:30 +0530

    GFG Multiply two linked lists

---
+++ LinkedList/Medium/multiply_Two_List.cpp
@@ -0,0 +1,25 @@
+
+
+// https://practice.geeksforgeeks.org/problems/multiply-two-linked-lists/1?page=3&category[]=Linked%20List&sortBy=submissions
+
+
+long long  multiplyTwoLists (Node* l1, Node* l2)
+{
+  long long  ans1=0;
+  long long  ans2=0;
+  long long  n=1000000007;
+  while(l1 || l2){
+      if(l1){
+          ans1=(ans1*10)%n+l1->data;
+          l1=l1->next;
+      }
+      if(l2){
+          ans2=(ans2*10)%n+l2->data;
+          l2=l2->next;
+      }
+  }
+  
+ // cout<<ans1<< " "<<ans2<<endl;
+  
+  return ((ans1)*(ans2))%n;
+}
