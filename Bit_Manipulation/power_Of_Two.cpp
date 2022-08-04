952947e1ced60e4e5a934f241a0ce36b4ac0bc90
anni-257 <taraleaniket257@gmail.com>
2022-08-04 10:46:26 +0530

    Leetcode and GFG Power Of Two

--- Bit_Manipulation/power_Of_Two.cpp
+++ Bit_Manipulation/power_Of_Two.cpp
@@ -16,3 +16,19 @@
         
         return false;
     }
+    
+    // https://practice.geeksforgeeks.org/problems/power-of-2-1587115620/1
+    
+     // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n){
+        
+        
+        
+        if((n&(n-1))==0 && n!=0){
+            return true;
+        }
+        
+        else 
+            return false;
+        
+    }
