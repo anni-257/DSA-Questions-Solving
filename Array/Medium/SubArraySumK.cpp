d5a4485656dd771ccbbd592431e3dae52dd4b58a
anni-257 <taraleaniket257@gmail.com>
2022-08-15 11:15:16 +0530

    GFG Subarray with given sum

---
+++ Array/Medium/SubArraySumK.cpp
@@ -0,0 +1,39 @@
+
+// https://practice.geeksforgeeks.org/problems/subarray-with-given-sum-1587115621/1?page=1&sortBy=submissions
+
+vector<int> subarraySum(int arr[], int n, long long s)
+    {
+        int i=0, j=0;
+        long long sum=0;
+        vector<int>ans;
+        
+        while(j<n){
+            sum+=arr[j];
+            
+            if(sum<s){
+                j++;
+            }
+                
+            else if (sum>s){
+                while(sum>s && i<j){
+                    sum-=arr[i++];
+                }
+                
+                if(sum!=s){
+                    j++;
+                }
+            }
+            
+            if(sum==s){
+                ans.push_back(i+1);
+                ans.push_back(j+1);
+                return ans;
+            }
+            
+            
+            
+        }
+        
+        ans.push_back(-1);
+        return ans;
+    }
